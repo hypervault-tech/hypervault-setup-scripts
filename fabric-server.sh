5258a2ea3c0b2ec3fcf09b2391dedd04cdd4c74a
Li Xi <xl404@cam.ac.uk>
2019-04-17 13:49:32 +0100

    Added zshell and aliases

--- fabric-server.sh
+++ fabric-server.sh
@@ -3,6 +3,14 @@
 sudo apt update
 sudo apt-get install -y build-essential
 
+# Install zsh
+sudo apt install zsh
+sudo apt-get install powerline fonts-powerline
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+sudo chsh -s /bin/zsh
+
+
 # install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
@@ -53,3 +61,16 @@ source rebuild.sh
 cd ../../
 source restart.sh
 
+# Setup aliases
+ALIASES='
+# Prints docker containers in paged mode
+alias dps="docker ps | less -S"
+alias tls="tmux list-session"
+alias tks="tmux kill-session -t "
+alias tns="tmux new -s "
+alias tat="tmux attach -t"
+'
+
+echo "Setting up the following aliases"
+echo "$ALIASES"
+echo "$ALIASES" > ~/.bash_aliases
