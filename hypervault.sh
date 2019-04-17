7fbd936c9b4208737f8e715b572c7467e54a45de
Li Xi <xl404@cam.ac.uk>
2019-04-17 15:27:37 +0100

    Added nvm commands and npm dependencies

--- hypervault.sh
+++ hypervault.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 set -e
-# Download the SSL Credentials
+# Download the SSL Credentials first to save the github credentials
 git clone https://github.com/lixiii/hypervault-server-credentials.git ~/credentials
 # set hostname
 sudo hostnamectl set-hostname fabric-endpoint
+
+# use node 8.9.4 and install composer dependencies
+nvm install 8.9.4 
+npm i -g pm2 composer-cli composer-playground 
+
 git clone https://github.com/lixiii/hypervault
 cd hypervault 
+npm i commander
+
+# update and install all submodules
 git submodule init
 git submodule update
 git submodule sync
@@ -17,5 +25,7 @@ cd composer-rest-server/docker/
 source build.sh
 cd ../../
 source rebuild.sh
-cd ../../
-source restart.sh
+cd ../../dev-tools/
+source ./restartHypervault.sh
+
+# source restart.sh
